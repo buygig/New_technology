2697a6ea77a4ef99458ab426f4794b1995289c66
jzy <1271639879@qq.com>
2022-06-08 21:10:32 +0800

    commit

--- webRTC/player/index.js
+++ webRTC/player/index.js
@@ -1,38 +1,53 @@
-const audio = document.querySelector('audio');
-
-console.log(audio, 777)
+const input = document.querySelector('input');
 
 const record = document.querySelector(".record");
 const play = document.querySelector('.play');
 const msg = document.querySelector('.msg');
+const title = document.querySelector('.title');
+
+const fileReader = new FileReader();
+const audioContext = new AudioContext();
+
+let source = null;
+let file = null;
+record.addEventListener('click', () => {
+  input.click();
+  input.addEventListener("change", (e) => {
+    console.log(e)
+    file = input.files[0];
+    let html = `<span>${file.name}</span>`;
+    title.innerHTML = html;
+    source = playAudio(file);
+    source.start();
+    audioContext.suspend();
+
+  })
+})
+
 
 function playAudio(file) {
-  const fileReader = new FileReader();
-  const audioContext = new AudioContext();
+  const source = audioContext.createBufferSource();
   fileReader.readAsArrayBuffer(file);
-  fileReader.onload = () => {
-    audioContext.decodeAudioData(fileReader.result, (result) => {
-      //创建播放源
-      const source = audioContext.createBufferSource();
-      source.buffer = result;
-      //连接输出终端
-      source.connect(audioContext.destination);
-      //开始播放
-      source.start();
-    });
+  fileReader.onload = async () => {
+    const result = await audioContext.decodeAudioData(fileReader.result);
+    source.buffer = result;
+    source.connect(audioContext.destination);
   }
+  return source;
 };
 
+
 let playTag = false;
+let currentTime = null;
 
 play.addEventListener('click', () => {
   if (playTag) {
-    audio.pause();
+    audioContext.suspend();
     playTag = false
     msg.classList.remove('up')
     record.style["animation-play-state"] = "paused";
   } else {
-    audio.play()
+    audioContext.resume();
     playTag = true;
     msg.classList.add('up')
     record.style["animation-play-state"] = "running";
