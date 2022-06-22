16012659ddf4fb91288d8cf1388941c404c71214
jzy <1271639879@qq.com>
2022-06-22 22:18:12 +0800

    commit

---
+++ canvas/myFarbic/fabric.lite.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas') || document.createElement('canvas')
+
+const width = canvas.width;
+const height = canvas.height;
+
+canvas.style.width = `${width}px`;
+canvas.style.height = `${height}px`;
+
